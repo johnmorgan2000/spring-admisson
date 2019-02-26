b1bdef32ec6aa1a1a769d5310f2ebc9bc9e20a32
John <jmorgan@basecampcodingacademy.org>
2019-02-26 08:54:12 -0600

    Initializing the project

---
+++ src/main/java/com/john/cole/admission/admissionprocess/AdmissionProcessApplication.java
@@ -0,0 +1,13 @@
+package com.john.cole.admission.admissionprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionProcessApplication.class, args);
+	}
+
+}
